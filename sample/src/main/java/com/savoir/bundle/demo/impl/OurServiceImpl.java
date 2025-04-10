55c9783dfcef6dd0052e7caa4441700378b0aadb
jgoodyear <jgoodyear@apache.org>
2025-04-10 11:27:28 -0230

    Initial project

---
+++ sample/src/main/java/com/savoir/bundle/demo/impl/OurServiceImpl.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2012-2025 Savoir Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.savoir.bundle.demo.impl;
+
+import com.savoir.bundle.demo.api.OurService;
+import com.savoir.bundle.demo.utils.HelpfulUtil;
+
+public class OurServiceImpl implements OurService {
+
+    @Override
+    public void doTheThing() {
+        //
+    }
+
+    @Override
+    public String makeXml(String input) {
+        return HelpfulUtil.convertToFormat(input);
+    }
+}
